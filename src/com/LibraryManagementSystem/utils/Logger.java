a9729682362600eb6a6f07d9a776d31e153a98d0
archis1405 <archismandas1405@gmail.com>
2025-09-14 23:26:26 +0530

    Added the Logger util

--- src/com/LibraryManagementSystem/utils/Logger.java
+++ src/com/LibraryManagementSystem/utils/Logger.java
@@ -1,4 +1,24 @@
 package com.LibraryManagementSystem.utils;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Logger {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-ddHH:mm:ss");
+
+    private static void info(String message) {
+        log("INFO", message);
+    }
+
+    public static void error(String message) {
+        log("ERROR", message);
+    }
+
+    public static void warn(String message) {
+        log("WARN", message);
+    }
+
+    public static void log(String level , String message){
+        String timestamp = LocalDateTime.now().format(formatter);
+        System.out.println(String.format("[%s] %s - %s", timestamp, level, message));
+    }
 }
