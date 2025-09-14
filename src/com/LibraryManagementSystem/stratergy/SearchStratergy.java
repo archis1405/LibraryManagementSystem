265af7a75d78430e34c68df6e355da6a0216725f
archis1405 <archismandas1405@gmail.com>
2025-09-14 23:52:44 +0530

    Added the SearchStratergy interface

--- src/com/LibraryManagementSystem/stratergy/SearchStratergy.java
+++ src/com/LibraryManagementSystem/stratergy/SearchStratergy.java
@@ -1,4 +1,7 @@
 package com.LibraryManagementSystem.stratergy;
+import com.LibraryManagementSystem.models.Book;
+import java.util.*;
 
-public class SearchStratergy {
+public interface SearchStratergy {
+    List<Book> search(String query);
 }
--- src/com/LibraryManagementSystem/stratergy/TitleSearchStratergy.java
+++ src/com/LibraryManagementSystem/stratergy/TitleSearchStratergy.java
@@ -1,4 +1,15 @@
 package com.LibraryManagementSystem.stratergy;
 
+import com.LibraryManagementSystem.models.Book;
+import com.LibraryManagementSystem.repository.BookRepository;
+import java.util.*;
+
 public class TitleSearchStratergy {
+    private final BookRepository bookRepository;
+
+    public TitleSearchStratergy(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+
 }
