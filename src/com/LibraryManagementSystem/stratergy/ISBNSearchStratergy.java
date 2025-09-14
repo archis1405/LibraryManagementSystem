caca62f104387c081a01c999041287768d1c4acd
archis1405 <archismandas1405@gmail.com>
2025-09-15 00:09:24 +0530

    Added the ISBNSearch Stratergy

--- src/com/LibraryManagementSystem/stratergy/ISBNSearchStratergy.java
+++ src/com/LibraryManagementSystem/stratergy/ISBNSearchStratergy.java
@@ -1,4 +1,20 @@
 package com.LibraryManagementSystem.stratergy;
 
-public class ISBNSearchStratergy {
+import com.LibraryManagementSystem.models.Book;
+import com.LibraryManagementSystem.repository.BookRepository;
+import java.util.*;
+
+public class ISBNSearchStratergy implements SearchStratergy {
+
+    private final BookRepository bookRepository;
+
+    public ISBNSearchStratergy(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public List<Book> search(String query){
+        Optional<Book> book = bookRepository.findByIsbn(query);
+        return book.map(Collections::singletonList).orElseGet(Collections::emptyList);
+    }
 }
