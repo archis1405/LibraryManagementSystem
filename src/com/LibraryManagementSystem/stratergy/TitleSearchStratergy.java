19a03c04625fb50cc25a7c5bac446e1151ceaa36
archis1405 <archismandas1405@gmail.com>
2025-09-14 23:55:19 +0530

    Added the TitleSearch Stratergy

--- src/com/LibraryManagementSystem/stratergy/TitleSearchStratergy.java
+++ src/com/LibraryManagementSystem/stratergy/TitleSearchStratergy.java
@@ -4,11 +4,15 @@ import com.LibraryManagementSystem.models.Book;
 import com.LibraryManagementSystem.repository.BookRepository;
 import java.util.*;
 
-public class TitleSearchStratergy {
+public class TitleSearchStratergy implements SearchStratergy {
     private final BookRepository bookRepository;
 
     public TitleSearchStratergy(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    public List<Book> search(String query){
+        return bookRepository.findByTitle(query);
     }
 
 
